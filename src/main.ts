645fcea94c28055723e6967f91cc06d572f94a79
FranciscoSoares0 <fsoares751@gmail.com>
2025-04-24 22:09:25 +0100

    enable CORS for frontend

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const frontendUrl = app.get<string>('FRONTEND_URL');
+
   // Enable CORS
   app.enableCors({
-    origin: 'http://localhost:4200', // allow Angular
+    origin: frontendUrl, // allow Angular
     credentials: true, // if you're sending cookies or Authorization headers
   });
   app.useGlobalPipes(
